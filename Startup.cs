0075995abef07a27e11b6f944ef1b92ae8652823
Gillebeert <sadmingillebse@alfa.top.vlaanderen.be>
2020-02-10 11:24:40 +0100

    'Second'

--- Startup.cs
+++ Startup.cs
@@ -32,7 +32,7 @@ namespace Test_webapp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Agentschap Binnenlands Bestuur");
                 });
             });
         }
